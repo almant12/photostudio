a87f3221dee1bf5eb9a130ec5a3bbfb59d571757
almant12 <dokoalmant123@gmail.com>
2024-09-23 15:30:43 +0200

    verify cookie token

---
+++ src/app/api/(auth)/logout/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+    try {
+        const response = NextResponse.json({ message: 'success' }, { status: 200 });
+
+        response.cookies.set('Authorization', '');
+
+        return response;
+    } catch (error) {
+        return NextResponse.json({ message: 'error' }, { status: 500 });
+    }
+}
--- src/lib/checkAdmin.ts
+++ src/lib/checkAdmin.ts
@@ -1,9 +1,30 @@
 'use server';
 
 import { cookies } from 'next/headers';
+import { jwtVerify } from 'jose';
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
 
 export const checkAdminStatus = async () => {
   const cookieStore = cookies();
-  const adminToken = cookieStore.get('Authorization');
-  return adminToken ? true : false;
+  const token = cookieStore.get('Authorization')?.value;
+
+  if (!token) {
+      return false; // No token found in cookies
+  }
+
+  try {
+      // Convert to Uint8Array
+      const secretKey = new TextEncoder().encode(JWT_SECRET);
+      
+      // Verify the JWT
+      await jwtVerify(token, secretKey);
+      
+      // If verification succeeds, return true
+      return true;
+  } catch (error) {
+      console.error('Error verifying token:', error);
+      // If verification fails, return false
+      return false;
+  }
 };
