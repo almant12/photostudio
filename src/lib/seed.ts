65dacd89978f622be6b0d50d743472874a27ae58
almant12 <dokoalmant123@gmail.com>
2024-09-23 20:32:21 +0200

    create function for insert user

---
+++ src/lib/seed.ts
@@ -0,0 +1,34 @@
+import {PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+
+async function seed(){
+
+    //Insert user
+    await prisma.user.createMany({
+        data:[
+            {
+            id:1,
+            name:"Almant",
+            email:"admin1@gmail.com",
+            password:bcrypt.hashSync('12345678',8)
+        },
+        {
+            id:2,
+            name:"Ari",
+            email:"admin2@gmail.com",
+            password:bcrypt.hashSync('12345678',8)
+        }
+    ],skipDuplicates:true
+})}
+
+seed()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
