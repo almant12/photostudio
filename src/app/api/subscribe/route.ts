4d7dca1f5f614f327467c6a9bd2292a2777dec28
almant12 <dokoalmant123@gmail.com>
2024-09-24 09:40:34 +0200

    subscribe admin

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -1,1 +1,60 @@
 
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+
+const prisma = new PrismaClient();
+
+export async function GET(){
+    
+
+    const subscribe = await prisma.subscription.findMany({
+        where:{adminId:1},
+        include:{
+            admin:true,
+            user:true
+        }
+    })
+
+    return NextResponse.json(subscribe);
+}
+
+
+export async function POST(req:NextRequest){
+
+  const formData = await req.formData();
+  const adminId = formData.get('adminId');
+
+    // Initialize an errors collection
+  const errors: { [key: string]: string } = {};
+
+  // Check if adminId is provided
+  if (!adminId) {
+    errors.adminId = 'adminId is required';
+  } else if (isNaN(Number(adminId))) {
+    // Check if adminId is a number
+    errors.adminId = 'adminId must be a valid number';
+  }
+
+  // If there are errors, return them in the response
+  if (Object.keys(errors).length > 0) {
+    return NextResponse.json({ errors }, { status: 400 });
+  }
+
+  //check admin exist
+  const admin = await prisma.user.findUnique({
+    where:{id:parseInt(adminId)}
+  });
+
+  if(!admin || admin.role !== 'ADMIN'){
+    return NextResponse.json({'message':"Admin not found"},{status:404});
+  }
+
+
+  //check if request has maded before
+
+
+  //save admin to subscribe user
+
+
+}
