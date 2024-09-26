f2f907e34a798f4a286964aefee51f2eb76704a1
almant12 <dokoalmant123@gmail.com>
2024-09-26 17:32:12 +0200

    function for admin to delete any post

--- src/app/api/admin/post/delete/[postId]/route.ts
+++ src/app/api/admin/post/delete/[postId]/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse,NextRequest } from "next/server";
 import { PrismaClient } from '@prisma/client';
-import { authUser } from "lib/authUser";
 
 const prisma = new PrismaClient();
 
@@ -10,23 +9,12 @@ export async function DELETE(req:NextRequest,{params}:{params:{postId:string}}){
         const { postId } = params;
         const id = parseInt(postId);
 
-        //pass the req into function for validate the header token and return user payload
-        const {valid,user} = await authUser();
-
-
-        const authenticatedUser = user;
-
         const post = await prisma.post.findUnique({
           where: { id: id },
         });
 
         if (!post) {
           return NextResponse.json({ message: 'Post not found' }, { status: 404 });
-        }
-
-        //check if the authenticate user is the author of the post
-        if (post.authorId !== parseInt(authenticatedUser.id)){
-            return NextResponse.json({'message':'Unauthorized to delete this post'},{status:403});
         }
 
         await prisma.post.delete({
